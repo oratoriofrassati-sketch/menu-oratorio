fa6934dc9428ee47e0d27a380052b92624cd6db4
oratoriofrassati-sketch <oratoriofrassati@sacrafamigliamonza.it>
2026-06-14 00:14:52 +0200

    Corregge headless chromium

--- src/app/tv-image/route.tsx
+++ src/app/tv-image/route.tsx
@@ -13,7 +13,7 @@ export async function GET(request: Request) {
   const browser = await puppeteer.launch({
     args: chromium.args,
     executablePath: await chromium.executablePath(),
-    headless: chromium.headless,
+    headless: true,
     defaultViewport: {
       width: 1920,
       height: 1080,
